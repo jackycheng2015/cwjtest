d89dacaa35b177a285bf1b0b9ae53b4709f65a96
jackycheng2015 <jacky.cheng_tm@qq.com>
2015-09-03 17:40:35 +0800

    S1

--- Tencent/Sample.cs
+++ Tencent/Sample.cs
@@ -10,24 +10,32 @@ namespace MsgCryptTest
         static void Main(string[] args)
         {
             //公众平台上开发者设置的token, corpID, EncodingAESKey
-            string sToken = "weixin;
+            string sToken = "weixin";
             string sCorpID = "wxf2b43ae4acb3aa6b";
             string sEncodingAESKey = "5VAJ4SaTmWyPyMkEwgfC4UtYPK6ntm7iRn6UHBQ5A16";
 
+			/*
+			------------使用示例一：验证回调URL---------------
+			*企业开启回调模式时，企业号会向验证url发送一个get请求 
+			假设点击验证时，企业收到类似请求：
+			* GET /cgi-bin/wxpush?msg_signature=5c45ff5e21c57e6ad56bac8758b79b1d9ac89fd3&timestamp=1409659589&nonce=263014780&echostr=P9nAzCzyDtyTWESHep1vC5X9xho%2FqYX3Zpb4yKa9SKld1DsH3Iyt3tP3zNdtp%2B4RPcs8TgAE7OaBO%2BFZXvnaqQ%3D%3D 
+			* HTTP/1.1 Host: qy.weixin.qq.com
+
+			* 接收到该请求时，企业应			1.解析出Get请求的参数，包括消息体签名(msg_signature)，时间戳(timestamp)，随机数字串(nonce)以及公众平台推送过来的随机加密字符串(echostr),
+			这一步注意作URL解码。
+			2.验证消息体签名的正确性 
+			3.解密出echostr原文，将原文当作Get请求的response，返回给公众平台
+			第2，3步可以用公众平台提供的库函数VerifyURL来实现。
+			*/
+			
             Tencent.WXBizMsgCrypt wxcpt = new Tencent.WXBizMsgCrypt(sToken, sEncodingAESKey, sCorpID);
-            /*
-             * 假定公众平台上开发者设置的Token
-            1. 验证回调URL
-             * 点击验证时，企业收到类似请求：
-             * GET /cgi-bin/wxpush?msg_signature=5c45ff5e21c57e6ad56bac8758b79b1d9ac89fd3&timestamp=1409659589&nonce=263014780&echostr=P9nAzCzyDtyTWESHep1vC5X9xho%2FqYX3Zpb4yKa9SKld1DsH3Iyt3tP3zNdtp%2B4RPcs8TgAE7OaBO%2BFZXvnaqQ%3D%3D 
-             * HTTP/1.1 Host: qy.weixin.qq.com
-             * 接收到该请求时，企业应1.先验证签名的正确性 2. 解密出echostr原文。
-             * 以上两步用verifyURL完成
-            */
-            //解析出url上的参数值如下：
+            // string sVerifyMsgSig = HttpUtils.ParseUrl("msg_signature");
             string sVerifyMsgSig = "5c45ff5e21c57e6ad56bac8758b79b1d9ac89fd3";
-            string sVerifyTimeStamp = "1409659589";
+            // string sVerifyTimeStamp = HttpUtils.ParseUrl("timestamp");
+			string sVerifyTimeStamp = "1409659589";
+			// string sVerifyNonce = HttpUtils.ParseUrl("nonce");
             string sVerifyNonce = "263014780";
+			// string sVerifyEchoStr = HttpUtils.ParseUrl("echostr");
             string sVerifyEchoStr = "P9nAzCzyDtyTWESHep1vC5X9xho/qYX3Zpb4yKa9SKld1DsH3Iyt3tP3zNdtp+4RPcs8TgAE7OaBO+FZXvnaqQ==";
             int ret = 0;
             string sEchoStr = "";
@@ -35,71 +43,90 @@ namespace MsgCryptTest
             if (ret != 0)
             {
                 System.Console.WriteLine("ERR: VerifyURL fail, ret: " + ret );
-                string input3 = System.Console.ReadLine();
                 return;
-            }
-            System.Console.WriteLine(sEchoStr);
+            }	
+			//ret==0表示验证成功，sEchoStr参数表示明文，用户需要将sEchoStr作为get请求的返回参数，返回给企业号。
+			// HttpUtils.SetResponse(sEchoStr);
 
-             /* 2. 对用户回复的数据进行解密。
-             * 用户回复消息或者点击事件响应时，企业会收到回调消息，假设企业收到的推送消息：
-             * 	POST /cgi-bin/wxpush? msg_signature=477715d11cdb4164915debcba66cb864d751f3e6&timestamp=1409659813&nonce=1372623149 HTTP/1.1
-	            Host: qy.weixin.qq.com
-                Content-Length: 613
-             *
-             * 	<xml>
-	                <ToUserName><![CDATA[wx5823bf96d3bd56c7]]></ToUserName>
-	                <Encrypt><![CDATA[RypEvHKD8QQKFhvQ6QleEB4J58tiPdvo+rtK1I9qca6aM/wvqnLSV5zEPeusUiX5L5X/0lWfrf0QADHHhGd3QczcdCUpj911L3vg3W/sYYvuJTs3TUUkSUXxaccAS0qhxchrRYt66wiSpGLYL42aM6A8dTT+6k4aSknmPj48kzJs8qLjvd4Xgpue06DOdnLxAUHzM6+kDZ+HMZfJYuR+LtwGc2hgf5gsijff0ekUNXZiqATP7PF5mZxZ3Izoun1s4zG4LUMnvw2r+KqCKIw+3IQH03v+BCA9nMELNqbSf6tiWSrXJB3LAVGUcallcrw8V2t9EL4EhzJWrQUax5wLVMNS0+rUPA3k22Ncx4XXZS9o0MBH27Bo6BpNelZpS+/uh9KsNlY6bHCmJU9p8g7m3fVKn28H3KDYA5Pl/T8Z1ptDAVe0lXdQ2YoyyH2uyPIGHBZZIs2pDBS8R07+qN+E7Q==]]></Encrypt>
-	                <AgentID><![CDATA[218]]></AgentID>
-                </xml>
-             */
+
+			
+			/*
+			------------使用示例二：对用户回复的消息解密---------------
+			用户回复消息或者点击事件响应时，企业会收到回调消息，此消息是经过公众平台加密之后的密文以post形式发送给企业，密文格式请参考官方文档
+			假设企业收到公众平台的回调消息如下：
+			POST /cgi-bin/wxpush? msg_signature=477715d11cdb4164915debcba66cb864d751f3e6&timestamp=1409659813&nonce=1372623149 HTTP/1.1
+			Host: qy.weixin.qq.com
+			Content-Length: 613
+			<xml>			<ToUserName><![CDATA[wx5823bf96d3bd56c7]]></ToUserName><Encrypt><![CDATA[RypEvHKD8QQKFhvQ6QleEB4J58tiPdvo+rtK1I9qca6aM/wvqnLSV5zEPeusUiX5L5X/0lWfrf0QADHHhGd3QczcdCUpj911L3vg3W/sYYvuJTs3TUUkSUXxaccAS0qhxchrRYt66wiSpGLYL42aM6A8dTT+6k4aSknmPj48kzJs8qLjvd4Xgpue06DOdnLxAUHzM6+kDZ+HMZfJYuR+LtwGc2hgf5gsijff0ekUNXZiqATP7PF5mZxZ3Izoun1s4zG4LUMnvw2r+KqCKIw+3IQH03v+BCA9nMELNqbSf6tiWSrXJB3LAVGUcallcrw8V2t9EL4EhzJWrQUax5wLVMNS0+rUPA3k22Ncx4XXZS9o0MBH27Bo6BpNelZpS+/uh9KsNlY6bHCmJU9p8g7m3fVKn28H3KDYA5Pl/T8Z1ptDAVe0lXdQ2YoyyH2uyPIGHBZZIs2pDBS8R07+qN+E7Q==]]></Encrypt>
+			<AgentID><![CDATA[218]]></AgentID>
+			</xml>
+
+			企业收到post请求之后应该			1.解析出url上的参数，包括消息体签名(msg_signature)，时间戳(timestamp)以及随机数字串(nonce)
+			2.验证消息体签名的正确性。
+			3.将post请求的数据进行xml解析，并将<Encrypt>标签的内容进行解密，解密出来的明文即是用户回复消息的明文，明文格式请参考官方文档
+			第2，3步可以用公众平台提供的库函数DecryptMsg来实现。
+			*/
+			
+			// string sReqMsgSig = HttpUtils.ParseUrl("msg_signature");
             string sReqMsgSig = "477715d11cdb4164915debcba66cb864d751f3e6";
+			// string sReqTimeStamp = HttpUtils.ParseUrl("timestamp");
             string sReqTimeStamp = "1409659813";
+			// string sReqNonce = HttpUtils.ParseUrl("nonce");
             string sReqNonce = "1372623149";
+			// Post请求的密文数据
+			// string sReqData = HttpUtils.PostData();
             string sReqData = "<xml><ToUserName><![CDATA[wx5823bf96d3bd56c7]]></ToUserName><Encrypt><![CDATA[RypEvHKD8QQKFhvQ6QleEB4J58tiPdvo+rtK1I9qca6aM/wvqnLSV5zEPeusUiX5L5X/0lWfrf0QADHHhGd3QczcdCUpj911L3vg3W/sYYvuJTs3TUUkSUXxaccAS0qhxchrRYt66wiSpGLYL42aM6A8dTT+6k4aSknmPj48kzJs8qLjvd4Xgpue06DOdnLxAUHzM6+kDZ+HMZfJYuR+LtwGc2hgf5gsijff0ekUNXZiqATP7PF5mZxZ3Izoun1s4zG4LUMnvw2r+KqCKIw+3IQH03v+BCA9nMELNqbSf6tiWSrXJB3LAVGUcallcrw8V2t9EL4EhzJWrQUax5wLVMNS0+rUPA3k22Ncx4XXZS9o0MBH27Bo6BpNelZpS+/uh9KsNlY6bHCmJU9p8g7m3fVKn28H3KDYA5Pl/T8Z1ptDAVe0lXdQ2YoyyH2uyPIGHBZZIs2pDBS8R07+qN+E7Q==]]></Encrypt><AgentID><![CDATA[218]]></AgentID></xml>";
-            string sMsg = "";  //解析之后的明文
+            string sMsg = "";  // 解析之后的明文
             ret = wxcpt.DecryptMsg(sReqMsgSig, sReqTimeStamp, sReqNonce, sReqData, ref sMsg);
             if (ret != 0)
             {
-                System.Console.WriteLine("ERR: Decrypt fail, ret: " + ret);
+                System.Console.WriteLine("ERR: Decrypt Fail, ret: " + ret);
                 return;
             }
-            System.Console.WriteLine(sMsg);
+			// ret==0表示解密成功，sMsg表示解密之后的明文xml串
+			// TODO: 对明文的处理
+			// For example:
+			XmlDocument doc = new XmlDocument();
+            doc.LoadXml(sMsg);
+            XmlNode root = doc.FirstChild;
+            string content = root["Content"].InnerText;
+			System.Console.WriteLine(content);
+			// ...
+			// ...
+			
+			
+			/*
+			------------使用示例三：企业回复用户消息的加密---------------
+			企业被动回复用户的消息也需要进行加密，并且拼接成密文格式的xml串。
+			假设企业需要回复用户的明文如下：
+			<xml>
+			<ToUserName><![CDATA[mycreate]]></ToUserName>
+			<FromUserName><![CDATA[wx5823bf96d3bd56c7]]></FromUserName>
+			<CreateTime>1348831860</CreateTime>
+			<MsgType><![CDATA[text]]></MsgType>
+			<Content><![CDATA[this is a test]]></Content>
+			<MsgId>1234567890123456</MsgId>
+			<AgentID>128</AgentID>
+			</xml>
 
-
-            /*
-             * 3. 企业回复用户消息也需要加密和拼接xml字符串。
-             * 假设企业需要回复用户的消息为：
-             * 		<xml>
-             * 		<ToUserName><![CDATA[mycreate]]></ToUserName>
-             * 		<FromUserName><![CDATA[wx5823bf96d3bd56c7]]></FromUserName>
-             * 		<CreateTime>1348831860</CreateTime>
-                    <MsgType><![CDATA[text]]></MsgType>
-             *      <Content><![CDATA[this is a test]]></Content>
-             *      <MsgId>1234567890123456</MsgId>
-             *      <AgentID>128</AgentID>
-             *      </xml>
-             * 生成xml格式的加密消息过程为：
-             */
-            string sRespData = "<xml><ToUserName><![CDATA[mycreate]]></ToUserName><FromUserName><![CDATA[wx582测试一下中文的情况，消息长度是按字节来算的396d3bd56c7]]></FromUserName><CreateTime>1348831860</CreateTime><MsgType><![CDATA[text]]></MsgType><Content><![CDATA[this is这是一个中文测试 a test]]></Content><MsgId>1234567890123456</MsgId><AgentID>128</AgentID></xml>";
+			为了将此段明文回复给用户，企业应：			1.自己生成时间时间戳(timestamp),随机数字串(nonce)以便生成消息体签名，也可以直接用从公众平台的post url上解析出的对应值。
+			2.将明文加密得到密文。	3.用密文，步骤1生成的timestamp,nonce和企业在公众平台设定的token生成消息体签名。			4.将密文，消息体签名，时间戳，随机数字串拼接成xml格式的字符串，发送给企业。
+			以上2，3，4步可以用公众平台提供的库函数EncryptMsg来实现。
+			*/
+			// 需要发送的明文
+            string sRespData = "<xml><ToUserName><![CDATA[mycreate]]></ToUserName><FromUserName><![CDATA[wx582396d3bd56c7]]></FromUserName><CreateTime>1348831860</CreateTime><MsgType><![CDATA[text]]></MsgType><Content><![CDATA[this is a test]]></Content><MsgId>1234567890123456</MsgId><AgentID>128</AgentID></xml>";
             string sEncryptMsg = ""; //xml格式的密文
             ret = wxcpt.EncryptMsg(sRespData, sReqTimeStamp, sReqNonce, ref sEncryptMsg);
-            System.Console.WriteLine("sEncryptMsg");
-            System.Console.WriteLine(sEncryptMsg);
+			if( ret!=0 )
+			{
+				System.Console.WriteLine("ERR: EncryptMsg Fail, ret: " + ret);
+				return;
+			}
+			// TODO:
+			// 加密成功，企业需要将加密之后的sEncryptMsg返回
+			// HttpUtils.SetResponse(sEncryptMsg);
 
-            /*测试：
-             * 将sEncryptMsg解密看看是否是原文
-             * */
-            XmlDocument doc = new XmlDocument();
-            doc.LoadXml(sEncryptMsg);
-            XmlNode root = doc.FirstChild;
-            string sig = root["MsgSignature"].InnerText;
-            string enc = root["Encrypt"].InnerText;
-            string timestamp = root["TimeStamp"].InnerText;
-            string nonce = root["Nonce"].InnerText;
-            string stmp = "";
-            ret = wxcpt.DecryptMsg(sig, timestamp, nonce, sEncryptMsg, ref stmp);
-            System.Console.WriteLine("stemp");
-            System.Console.WriteLine(stmp + ret);
+
             return;
         }
     }
--- Tencent/WXBizMsgCrypt.cs
+++ Tencent/WXBizMsgCrypt.cs
@@ -1,15 +1,4 @@
-﻿/*----------------------------------------------------------------
-    文件名：WXBizMsgCrypt.cs
-    文件功能描述：加解密算法
-    
-    
-    创建标识：Senparc - 20150313
-    
-    修改标识：Senparc - 20150313
-    修改描述：整理接口
-----------------------------------------------------------------*/
-
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -29,7 +18,7 @@ using System.Security.Cryptography;
 //-40010 :  base64解密异常
 namespace Tencent
 {
-   public class WXBizMsgCrypt
+    class WXBizMsgCrypt
     {
         string m_sToken;
         string m_sEncodingAESKey;
