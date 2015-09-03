5d6504bf4b146ca2f4a10b42cf2f331a6546c09a
jackycheng2015 <jacky.cheng_tm@qq.com>
2015-09-03 14:27:32 +0800

    S1

--- CheckSignature.cs
+++ CheckSignature.cs
@@ -6,62 +6,7 @@ using Tencent;
 
 namespace Sample_1
 {
-    //public class CheckSignature
-    //{
-    //    /// <summary>
-    //    /// 在网站没有提供Token（或传入为null）的情况下的默认Token。
-    //    /// </summary>
-    //    public const string Token = "weixin";
-    //    /// <summary>
-    //    /// 在网站没有提供EncodingAESKey（或传入为null）的情况下的默认Token，建议在网站中进行配置。
-    //    /// </summary>
-    //    public const string EncodingAESKey = "5VAJ4SaTmWyPyMkEwgfC4UtYPK6ntm7iRn6UHBQ5A16";
-    //    ///// <summary>
-    //    ///// 在网站没有提供CorpId（或传入为null）的情况下的默认Token，建议在网站中进行配置。
-    //    ///// </summary>
-    //    //public const string CorpId = "Senparc";
 
-    //    /// <summary>
-    //    /// 检查签名是否正确
-    //    /// </summary>
-    //    /// <param name="signature"></param>
-    //    /// <param name="timestamp"></param>
-    //    /// <param name="nonce"></param>
-    //    /// <param name="token"></param>
-    //    /// <returns></returns>
-    //    public static bool Check(string signature, string timestamp, string nonce, string token)
-    //    {
-    //        return signature == GetSignature(timestamp, nonce, token);
-    //    }
-
-    //    /// <summary>
-    //    /// 返回正确的签名
-    //    /// </summary>
-    //    /// <param name="timestamp"></param>
-    //    /// <param name="nonce"></param>
-    //    /// <param name="token"></param>
-    //    /// <returns></returns>
-    //    public static string GetSignature(string timestamp, string nonce, string token = Token)
-    //    {
-    //        token = token ?? Token;
-    //        string[] arr = new[] { token, timestamp, nonce }.OrderBy(z => z).ToArray();
-    //        string arrString = string.Join("", arr);
-    //        System.Security.Cryptography.SHA1 sha1 = System.Security.Cryptography.SHA1.Create();
-    //        byte[] sha1Arr = sha1.ComputeHash(Encoding.UTF8.GetBytes(arrString));
-    //        StringBuilder enText = new StringBuilder();
-    //        foreach (var b in sha1Arr)
-    //        {
-    //            enText.AppendFormat("{0:x2}", b);
-    //        }
-    //        return enText.ToString();
-    //    }
-    //}
-
-
-
-
-
-{
     public static class Signature
     {
         /// <summary>
